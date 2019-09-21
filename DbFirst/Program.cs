5567727eefdde2029d0346bfa99e7b11b8979046
dodinhnguyen <nguyendo891@yahoo.com>
2019-09-21 17:27:58 +0200

    Add project files.

---
+++ DbFirst/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFirst
+{
+    public enum Level : byte
+    {
+        Beginner = 1,
+        Intermediate = 2,
+        Advanced = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var course = new Course();
+            course.Level = Level.Beginner;
+        }
+    }
+}
